23889446c49f8544a046c8d60731a7071adf3f69
Matheus Pozett <maa_souza11@hotmail.com>
2025-03-11 13:51:55 -0300

    req02

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import Header from './components/Header';
+import SolarSystem from './components/SolarSystem';
 
 function App() {
   return (
     <main>
       <Header />
+      <SolarSystem />
     </main>
   );
 }
---
+++ src/components/SolarSystem.tsx
@@ -0,0 +1,7 @@
+function SolarSystem() {
+  return (
+    <div data-testid="solar-system">SolarSystem</div>
+  );
+}
+
+export default SolarSystem;
